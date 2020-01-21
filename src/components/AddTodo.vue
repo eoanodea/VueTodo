c9345ee02d8714097d3b35057a04647775ac27ba
Eoan O Dea <odeaeoan@gmail.com>
2020-01-21 14:24:17 +0000

    Created VueTodo App where you can add todos to a list

---
+++ src/components/AddTodo.vue
@@ -0,0 +1,68 @@
+<template>
+    <span>
+        <b-button v-b-modal.modal-1 variant="success" class="float-right">Add Todo</b-button>
+
+        <b-modal id="modal-1" title="New Todo" @ok="handleOk">
+            <b-form @submit="onSubmit" @submit.stop.prevent="onSubmit">
+                <b-form-group
+                    id="todo"
+                    label="Todo Name"
+                    label-for="todo"
+                    description="Enter the todo you would like to add."
+                    invalid-feedback="Todo is required"
+                >
+                    <b-form-input
+                    id="todo"
+                    v-model="todo.name"
+                    type="text"
+                    required
+                    placeholder="Enter Todo"
+                    ></b-form-input>
+                </b-form-group>
+            </b-form>
+        </b-modal>
+    </span>
+</template>
+
+<script>
+/* eslint-disable no-console */
+// import Todo from './components/Todo'
+
+export default {
+    data() {
+        return {
+            todo: {
+                name: "",
+                done: false
+            }
+        }
+    }, 
+    components: {
+        // Todo
+    },
+    methods: {
+        
+        validateForm() {
+            if(this.todo.name == "") return false
+            return true
+        },
+
+        handleOk(bvModalEvt) {
+            bvModalEvt.preventDefault()
+            this.onSubmit()
+        },
+
+        onSubmit() {
+            if(this.validateForm()) {
+                console.log('validated!!', this.todo.name)
+                this.$emit("added-todo", this.todo)
+                // this.todo.name = ""
+                this.$nextTick(() => {
+                    this.$bvModal.hide('modal-1')
+                })
+
+            } else return
+        }
+    }
+}
+</script>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,48 @@
+<template>
+    <b-card
+        title="Todos"
+
+    >
+        <add-todo v-on:added-todo="addNewTodo"></add-todo>
+        <b-table striped hover :items="todos"></b-table>
+    </b-card>
+</template>
+
+<script>
+
+/* eslint-disable no-console */
+
+// import Todo from './components/Todo'
+import AddTodo from './AddTodo'
+
+export default {
+
+    data() {
+        return {
+            todos: [
+                {id: 0, name: "Learn Vue.js", done: false}
+            ]
+        }
+    }, 
+    components: {
+        AddTodo
+    },
+    methods: {
+        addNewTodo(todo) {
+            console.log('new todo!!', todo)
+            let {todos} = this
+            
+            const todoIndex = todos.length
+            
+            let newTodo = {
+                id: todoIndex,
+                name: todo.name,
+                done: todo.done
+            }
+
+            todos.push(newTodo)
+            this.todos = todos
+        }
+    }
+}
+</script>
